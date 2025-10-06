8441311466d33c1513829fc67b867e5c498983f3
NawarTurk <nawar.turk@mail.concordia.ca>
2025-10-05 21:04:49 -0400

    add: llm prompt results using interview_question column for prompt1

--- models/prompting/run_hf_api.py
+++ models/prompting/run_hf_api.py
@@ -9,40 +9,42 @@ prompts_path    = os.path.join("..", "..", "prompts")
 
 # NOTE: you need to check if a certain model is deployed by any HF inference provider
 MODEL_REGISTRY = {
-    # LLaMA family
+    # ---- LLaMA family ----
     "llama-3.1-nemotron-253b": "nvidia/Llama-3_1-Nemotron-Ultra-253B-v1", # nebius
     "llama-3.1-405b-Instruct": "meta-llama/Llama-3.1-405B-Instruct",  # nebius
-    # "llama-3-405b-Intruct":    "meta-llama/Llama-3-405B-Instruct",  # ???
     "llama-3.3-70b-Instruct": "meta-llama/Llama-3.3-70B-Instruct",  # nebius 
 
-    # Qwen family
+    # ---- Qwen family ---- 
     "qwen3-coder-480b-Instruct": "Qwen/Qwen3-Coder-480B-A35B-Instruct",  # nebius 
 
     "qwen3-235b-instruct": "Qwen/Qwen3-235B-A22B-Instruct-2507", # nebius 
-    "qwen3-80b-instruct": "Qwen/Qwen3-Next-80B-A3B-Instruct", # novita try together 
+    "qwen3-80b-instruct": "Qwen/Qwen3-Next-80B-A3B-Instruct", # novita 
     "qwen3-32b-instruct": "Qwen/Qwen3-30B-A3B-Instruct-2507", # nebius 
 
-    # Other models
+    # ---- Mixtral ---- 
     "mixtral-8x22b-Instruct":   "mistralai/Mixtral-8x22B-Instruct-v0.1", # nscale  
     "mixtral-8x7b-Instruct": "mistralai/Mixtral-8x7B-Instruct-v0.1",  # together
-
+    
+    # ---- ther models ---- 
     # "gpt-oss-120b": "openai/gpt-oss-120b",  # returning empty replies  
-
     # "phi-2.7b":   "microsoft/phi-2",
     # "falcon-7b":  "tiiuae/falcon-7b-instruct",
 }
 
-llm_name       = "qwen3-80b-instruct"  
-provider       = "novita"  # nebius
-model_id       = MODEL_REGISTRY[llm_name]
-prompt_template = "01_t1_zs_re2.txt"  
+# ---- configuration ----
+llm_name       = "mixtral-8x7b-Instruct"  
+provider       = "together"  # nebius together nscale
+prompt_template = "01_t1_zs_re2.txt" 
+question_col =  "interview_question"  # options are: "question" "interview_question"
 
-prompt_name     = os.path.splitext(prompt_template)[0] 
-pred_col        = f"{llm_name}_{prompt_name}_{provider}"          # e.g., "qwen-1.8b_01_t1_zs_re2_nebius"
-out_path        = os.path.join(prediction_path, f"{pred_col}.csv")
+
+prompt_name = os.path.splitext(prompt_template)[0] 
+pred_col = f"{llm_name}_{prompt_name}_{provider}_{'question' if question_col == 'question' else 'interview-question'}"          # e.g., "qwen-1.8b_01_t1_zs_re2_nebius"
+out_path = os.path.join(prediction_path, f"{pred_col}.csv")
 
 # ---- auth & client ----
 hf_token = os.environ["HF_TOKEN"]  # token with “Make calls to Inference Providers”
+model_id = MODEL_REGISTRY[llm_name]
 client   = InferenceClient(model=model_id, token=hf_token, provider=provider)
 
 # load system message from prompts/
@@ -68,13 +70,13 @@ for i, row in tqdm(test_df.iterrows(), total=len(test_df), desc="Processing"):
     if pd.notna(row[pred_col]):    
         continue  
      
-    q = row["question"]
+    q = row[question_col]
     a = row["interview_answer"]
     try:
         p = classify(q, a)
         test_df.at[i, pred_col] = p
     except Exception:
-        time.sleep(0.5)
+        time.sleep(2)
         p = classify(q, a)
         test_df.at[i, pred_col] = p
 
